5bc7ad708d8fbb355cc12f77b3c04cf4bbd8d1d3
Ted Clayton <ted_clayton@hotmail.com>
2021-10-30 18:34:14 -0400

    Created python variables that match the categories on the form.

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -16,3 +16,15 @@ def detail(request, hero_id):
         'single_hero': single_hero
     }
     return render(request, 'superheroes/detail.html', context )
+
+    def create(request):
+        if request.method == "POST":
+            # save the form contents as a new db object
+            # return to index
+            name = request.POST.get('name')
+            alter_ego = request.POST.get('alter_ego')
+            primary = request.POST.get('primary')
+            secondary = request.POST.get('secondary')
+            catchphrase = request.POST.get('catchphrase')
+        else:
+            return render(request, 'superheroes/create.html')
